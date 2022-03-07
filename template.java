ea2fbbf9ff52dcf0b3f7353df0ea47a6de7c436c
SBBradley <sbbradley8@protonmail.com>
2022-03-07 17:00:27 +0900

    Added template files

---
+++ template.java
@@ -0,0 +1,18 @@
+package Templates;
+import java.util.*;
+import java.io.*;
+
+//What is this program for?
+//Date
+public class template {
+
+//Main method
+public static void main(String[] args) {
+
+}
+
+//What does this method do?
+public void method() {
+
+}
+}
---
+++ template.py
@@ -0,0 +1,8 @@
+#What is this program for?
+#Date
+
+#What does this function do?
+def newFunction():
+    print("hello from newFunction.")
+
+newFunction()

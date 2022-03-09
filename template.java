fb0fcc7d737a1b7bfbd1a0b2cc01b891d0683497
SBBradley <97514020+SBBradley@users.noreply.github.com>
2022-03-09 16:46:02 +0900

    Update template.java

--- template.java
+++ template.java
@@ -1,18 +1,18 @@
+//What is this program for?
+//Date
 package Templates;
 import java.util.*;
 import java.io.*;
 
-//What is this program for?
-//Date
 public class template {
 
 //Main method
 public static void main(String[] args) {
-
+newMethod();
 }
 
 //What does this method do?
-public void method() {
-
+public void newMethod() {
+System.out.prinln("Hello from newMethod.");
 }
 }
